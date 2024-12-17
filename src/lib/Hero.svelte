9969952daa269fd9505c8d718bf5a6636e0323c3
Pairadux <austincgause@gmail.com>
2024-12-17 15:20:41 -0500

    Feat: add Hero section

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { assets } from '$app/paths';
+</script>
+
+<section class="hero is-fullheight-with-navbar has-bg-img">
+	<div class="container hero-body is-flex is-flex-direction-column is-justify-content-center">
+		<div
+			class="has-text-centered is-flex is-flex-direction-column is-align-content-center is-justify-content-center"
+		>
+			<p class="is-size-1 is-great-vibes has-text-white">Welcome to<br />Dirty Beach Cleaning</p>
+			<figure class="image is-square mx-auto my-6">
+				<img src="{assets}/images/logo-with-sub.png" alt="" class="is-rounded" />
+			</figure>
+			<p class="subtitle is-4 is-size-5-touch has-text-weight-semibold has-text-white">
+				Providing professional Residential and Vacation Rental cleaning services in Franklin and
+				Wakulla counties.
+			</p>
+		</div>
+	</div>
+</section>
+
+<style>
+	.hero-body {
+		z-index: 100;
+	}
+
+	.has-bg-img {
+		position: relative;
+		overflow: hidden;
+	}
+
+	.has-bg-img::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-image: url('./images/beach_image.jpg');
+		background-size: cover;
+		background-position: center;
+		filter: brightness(50%);
+	}
+
+	.image {
+		height: 20rem;
+		width: 20rem;
+	}
+
+	.subtitle {
+		width: 32rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script lang="ts">
 	import Navbar from '$lib/Navbar.svelte';
+	import Hero from '$lib/Hero.svelte';
 	import Bio from '$lib/Bio.svelte';
 </script>
 <Navbar />
