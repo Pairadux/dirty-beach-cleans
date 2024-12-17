a6608df0d59ce5702cdcbf1d24c9b3d1aadff41e
Pairadux <austincgause@gmail.com>
2024-12-17 15:20:30 -0500

    Style: format files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script lang="ts">
-    import Navbar from "$lib/Navbar.svelte";
+	import Navbar from '$lib/Navbar.svelte';
 	import Bio from '$lib/Bio.svelte';
 </script>
 <Navbar />
