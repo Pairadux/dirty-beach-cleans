712e6340518c53577bbf8a0e2bdc45f0acf0553a
Pairadux <austincgause@gmail.com>
2024-12-17 16:39:26 -0500

    Fix: move navbar and footer to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-	import Navbar from '$lib/Navbar.svelte';
 	import Hero from '$lib/Hero.svelte';
 	import Bio from '$lib/Bio.svelte';
 	import Checklist from '$lib/Checklist.svelte';
-	import Footer from '$lib/Footer.svelte';
 </script>
 
-<Navbar />
 <Hero />
 <Bio />
 <Checklist />
-<Footer />
